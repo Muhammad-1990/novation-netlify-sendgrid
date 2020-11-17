c96d6cfc379d2632fd8fd4a3cfec5ac64dfafcef
Kube02 <Kube02@sableinternational.com>
2020-11-17 08:51:03 +0200

    redo because of compromised api key

---
+++ functions/sendmail.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+exports.handler = async (event, context, callback) => {
+    const data = JSON.parse(event.body)
+    const body = Object.keys(data).map((k) => {
+        return `${k}: ${data[k]}`
+    }).join("<br><br>");
+    const mail_to_send = {
+        to: process.env.SENDGRID_API_MAIL_TO,
+        from: process.env.SENDGRID_API_MAIL_FROM,
+        subject: 'New Entry from Website',
+        html: body,
+    };
+    try {
+        await sgMail.send(mail_to_send)
+        return {
+            statusCode: 200,
+            headers: { "Access-Control-Allow-Origin": "*" },
+            body: "Message sent successfully"
+        }
+    } catch (e) {
+        return {
+            statusCode: e.code,
+            body: e.message
+        }
+    }
+};
