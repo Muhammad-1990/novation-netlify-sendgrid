2d15bbb1856f98c83a47acd21499979361e940e0
Muhammad Ahmod <53302444+Muhammad-1990@users.noreply.github.com>
2023-09-01 16:31:22 +0200

    Update lcrSendEmail.js

--- functions/lcrSendEmail.js
+++ functions/lcrSendEmail.js
@@ -1,33 +1,48 @@
-var nodemailer = require('nodemailer');
+
 
 exports.handler = async (event, context, callback) => {
-   // console.log('in handler');
-   // const data = JSON.parse(event.body)
-    var email = "info@louwcooper.co.za";
-   // const body = Object.keys(data).map((k) => {
-  //      return `${k}: ${data[k]}`
-  //  }).join("<br><br>");
+   const nodemailer = require('nodemailer');
+    const data = JSON.parse(event.body)
+    const body = Object.keys(data).map((k) => {
+        return `${k}: ${data[k]}`
+    }).join("<br><br>");
 
     try {
-      const transportOptions = {
-            host: 'smtp.office365.com',
-            port: '587',
-            auth: { user: "info@louwcooper.co.za", pass: process.env.LCR_EMAIL_PASSWORD },
-            secureConnection: true,
-            tls: { ciphers: 'SSLv3' }
-        };
+       const transporter = nodemailer.createTransport({
+    host: 'smtp.office365.com',
+    port: 587,
+    auth: {
+        user: 'info@louwcooper.co.za',
+        pass: process.env.LCR_EMAIL_PASSWORD
+    }
+});
+
+// send email
+await transporter.sendMail({
+    from: 'info@louwcooper.co.za',
+    to: 'info@louwcooper.co.za',
+    subject: 'New Entry from Website',
+    text: body
+});
+       
+   //   const transportOptions = {
+    //        host: 'smtp.office365.com',
+    //        port: '587',
+    //        auth: { user: "info@louwcooper.co.za", pass: process.env.LCR_EMAIL_PASSWORD },
+    //        secureConnection: true,
+    //        tls: { ciphers: 'SSLv3' }
+    //    };
     
-        const mailTransport = nodemailer.createTransport(transportOptions);
-    console.log('sending');
-        await mailTransport.sendMail({
-            from: email,
-            to: email,
-            replyTo: email,
-            subject: "New Entry from Website",
-            html:"",
-            text: "test"
-        });
-      console.log('return');
+    //    const mailTransport = nodemailer.createTransport(transportOptions);
+
+     //   await mailTransport.sendMail({
+    //        from: email,
+     //       to: email,
+     //       replyTo: email,
+     //       subject: "New Entry from Website",
+     //       html:"",
+     //       text: "test"
+      //  });
         return {
             statusCode: 200,
             headers: { "Access-Control-Allow-Origin": "*" },
